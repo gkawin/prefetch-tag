c8cb62b4890a5be465fc54aa173506aa7c726771
Kawin Glomjai <kawin.glomjai@axia.life>
2018-06-11 14:55:32 +0700

    mono-repo

---
+++ packages/frontend/About.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class About extends React.Component {
+    public render() {
+        return (
+            <div>
+                <h2>About</h2>
+            </div>
+        );
+    }
+}
---
+++ packages/frontend/ChartBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ChartBar extends React.Component {
+    public render() {
+        return (
+            <div>
+                <h1> CHART BAR</h1>
+            </div>
+        );
+    }
+}
