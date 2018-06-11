1e12a0aed0fa09e992e1dd40d7af505f599b937b
Kawin Glomjai <kawin.glomjai@axia.life>
2018-06-11 17:19:25 +0700

    merge all

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -6,16 +6,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const prod = process.env.NODE_ENV.trim() === "production"
 
 module.exports = {
-    devtool: "source-map",
-    mode: "development",
-    entry:{ },
+    mode: prod ? "production" : "development",
+    entry:{
+        main: path.resolve(__dirname, 'src', 'index.tsx')
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js"
     },
     module: {
         rules: [
-            { test: /\.tsx?$/, exclude: /node_modules/, use: "ts-loader" }
+            { test: /\.tsx?$/, exclude: /node_modules/, use: { loader: "ts-loader", options: { happyPackMode: true } } }
         ]
     },
     resolve: {
